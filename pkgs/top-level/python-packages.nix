3029da4f83f99606f3b28dfcd0a4b76d62153c1c
Martin Weinelt <hexa@darmstadt.ccc.de>
2024-11-15 01:17:15 +0100

    python313Packages.hug: drop
    Marked broken since 2021, still pins falcon==2.0.0 and we are at falcon>4
    now.

--- pkgs/top-level/python-packages.nix
+++ pkgs/top-level/python-packages.nix
@@ -5972,8 +5972,6 @@ self: super: with self; {
 
   huey = callPackage ../development/python-modules/huey { };
 
-  hug = callPackage ../development/python-modules/hug { };
-
   huggingface-hub = callPackage ../development/python-modules/huggingface-hub { };
 
   huisbaasje-client = callPackage ../development/python-modules/huisbaasje-client { };
